1eb57c06cb17d49394558fbdb751620ea94c90e1
Rafael Tamayo Asensio <rtamayo.uk@gmail.com>
2019-08-26 13:37:39 +0100

    Show /played and takes the screenshot depending on the level

--- DingPics.lua
+++ DingPics.lua
@@ -1,8 +1,18 @@
 local frame = CreateFrame("Frame")
-frame:Hide()
 frame:RegisterEvent("PLAYER_LEVEL_UP")
 frame:SetScript("OnEvent", function (self, event)
-	-- wait a bit so the yellow animation appears 300ms seems good
-	C_Timer.After(.3, Screenshot)
-end
-)
+	RequestTimePlayed()
+	C_Timer.After((UnitLevel("player") - 1) * (2.0 / 60.0), function()
+		SaveView(4) -- Saves current camera position
+		C_CVar.SetCVar("cameraViewBlendStyle", 2) -- Allow the camera to move instantly
+		SetView(5) -- Switches to default camera position
+		frame:SetScript("OnUpdate", function(self, elapsed) -- Waits for next frame
+			Screenshot()
+			frame:SetScript("OnUpdate", function(self, elapsed)
+				SetView(4) -- Restores previous camera position
+				C_CVar.SetCVar("cameraViewBlendStyle", 1) -- Restores camera behavior
+				frame:SetScript("OnUpdate", nil)
+			end)
+		end)
+	end)
+end)
