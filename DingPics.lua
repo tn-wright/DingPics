a5457affec03a7fbaa3fe938f4c3b4074417195d
Travis <travisnwright@gmail.com>
2020-04-30 01:18:45 -0400

    Remove /played, hide UI
    Instead of displaying /played information, hide all elements of the UI

--- DingPics.lua
+++ DingPics.lua
@@ -1,7 +1,7 @@
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("PLAYER_LEVEL_UP")
 frame:SetScript("OnEvent", function (self, event)
-	RequestTimePlayed()
+	UIParent:Hide() -- Hide the UI
 	C_Timer.After((UnitLevel("player") - 1) * (2.0 / 60.0), function()
 		SaveView(4) -- Saves current camera position
 		C_CVar.SetCVar("cameraViewBlendStyle", 2) -- Allow the camera to move instantly
@@ -11,6 +11,7 @@ frame:SetScript("OnEvent", function (self, event)
 			frame:SetScript("OnUpdate", function(self, elapsed)
 				SetView(4) -- Restores previous camera position
 				C_CVar.SetCVar("cameraViewBlendStyle", 1) -- Restores camera behavior
+				UIParent:Show() -- Restores the UI
 				frame:SetScript("OnUpdate", nil)
 			end)
 		end)
