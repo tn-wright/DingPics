a6f3669caee50afcbb7a3bc4e08d646b7ec8b414
Travis <travisnwright@gmail.com>
2020-05-01 17:31:23 -0400

    Add conditional open and close for bags, map, and character frame. Add testing command

--- DingPics.lua
+++ DingPics.lua
@@ -1,7 +1,22 @@
 local frame = CreateFrame("Frame")
+local mapShown = false
+local bagsShown = false
+local characterFrameShown = false
 frame:RegisterEvent("PLAYER_LEVEL_UP")
 frame:SetScript("OnEvent", function (self, event)
-	UIParent:Hide() -- Hide the UI
+	RequestTimePlayed()
+	if WorldMapFrame:IsShown() then 
+		mapShown = true 
+		WorldMapFrame:Hide()
+	end
+	if CharacterFrame:IsShown() then 
+		characterFrameShown = true
+		CharacterFrame:Hide()
+	end
+	if ElvUI_ContainerFrame:IsShown() then 
+		bagsShown = true 
+		ElvUI_ContainerFrame:Hide()
+	end
 	C_Timer.After((UnitLevel("player") - 1) * (2.0 / 60.0), function()
 		SaveView(4) -- Saves current camera position
 		C_CVar.SetCVar("cameraViewBlendStyle", 2) -- Allow the camera to move instantly
@@ -11,9 +26,44 @@ frame:SetScript("OnEvent", function (self, event)
 			frame:SetScript("OnUpdate", function(self, elapsed)
 				SetView(4) -- Restores previous camera position
 				C_CVar.SetCVar("cameraViewBlendStyle", 1) -- Restores camera behavior
-				UIParent:Show() -- Restores the UI
+				if mapShown then WorldMapFrame:Show() end
+				if bagsShown then ElvUI_ContainerFrame:Show() end
+				if characterFrameShown then CharacterFrame:Show() end
 				frame:SetScript("OnUpdate", nil)
 			end)
 		end)
 	end)
 end)
+
+SLASH_DING1 = "/ding"
+SlashCmdList["DING"] = function(msg)
+	RequestTimePlayed()
+	if WorldMapFrame:IsShown() then 
+		mapShown = true 
+		WorldMapFrame:Hide()
+	end
+	if CharacterFrame:IsShown() then 
+		characterFrameShown = true
+		CharacterFrame:Hide()
+	end
+	if ElvUI_ContainerFrame:IsShown() then 
+		bagsShown = true 
+		ElvUI_ContainerFrame:Hide()
+	end
+	C_Timer.After((UnitLevel("player") - 1) * (2.0 / 60.0), function()
+		SaveView(4) -- Saves current camera position
+		C_CVar.SetCVar("cameraViewBlendStyle", 2) -- Allow the camera to move instantly
+		SetView(5) -- Switches to default camera position
+		frame:SetScript("OnUpdate", function(self, elapsed) -- Waits for next frame
+			Screenshot()
+			frame:SetScript("OnUpdate", function(self, elapsed)
+				SetView(4) -- Restores previous camera position
+				C_CVar.SetCVar("cameraViewBlendStyle", 1) -- Restores camera behavior
+				if mapShown then WorldMapFrame:Show() end
+				if bagsShown then ElvUI_ContainerFrame:Show() end
+				if characterFrameShown then CharacterFrame:Show() end
+				frame:SetScript("OnUpdate", nil)
+			end)
+		end)
+	end)
+end
